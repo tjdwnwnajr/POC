be1c34491970bd3b4e32ae74771488cfc0b4dfb6
성주 <tjdwnwnajr@gmail.com>
2026-02-13 20:33:11 +0900

    02-13 꿈작업중

--- Assets/Scripts/Tools/CreateRock.cs
+++ Assets/Scripts/Tools/CreateRock.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using UnityEngine;
-
+using Cinemachine;
 public class CreateRock : MonoBehaviour
 {
     [SerializeField] private GameObject rockPrefab;
@@ -8,10 +8,14 @@ public class CreateRock : MonoBehaviour
     private bool canCreate = false;
     [HideInInspector]public bool isCreated = false;
     [SerializeField] private float coolTime = 5f;
+    [SerializeField] private CinemachineVirtualCamera vcam;
+    [SerializeField] private Transform defaultFollowTarget;
+
     private bool endCreate = false;
     private void Start()
     {
         canCreate = false;
+        defaultFollowTarget = vcam.Follow;
     }
 
     private void Update()
@@ -29,9 +33,17 @@ public class CreateRock : MonoBehaviour
     }
     IEnumerator SpawnRock()
     {
-        Instantiate(rockPrefab, rockSpawnPoint, Quaternion.identity);
+        InputManager.DeactivatePlayerControls();
+        GameObject rock = Instantiate(rockPrefab, rockSpawnPoint, Quaternion.identity);
         isCreated = true;
-        yield return new WaitForSeconds(coolTime);
+        if (vcam != null && rock != null)
+        {
+            vcam.Follow = rock.transform;
+        }
+        yield return new WaitForSeconds(4f);
+        vcam.Follow = defaultFollowTarget;
+        InputManager.ActivatePlayerControls();
+        yield return new WaitForSeconds(coolTime-4f);
         isCreated = false;
     }
     private void OnTriggerEnter2D(Collider2D collision)
